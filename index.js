1ffd631de978e9146869390bcd244e7f458325d0
nik19ta <nikhvatov19@gmail.com>
2021-01-12 06:47:40 +0300

    v-0.1

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const db = require('./db')
+
+const app = express();
+const jsonParser = bodyParser.json()
+const html = './public/html';
+const port = 3030;
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(html + '/index.html'));
+});
+
+app.post('/check', jsonParser, function (req, res) {
+    if (req.body.link == "") res.send({
+        "data": null
+    });
+
+    let data = db.search(req.body.link);
+    if (data == null) {
+        res.send({
+            "data": null
+        });
+    } else {
+        res.send({
+            "data": data
+        })
+    }
+})
+
+
+app.post('/new', jsonParser, function (req, res) {
+    function make_short_link(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    if (req.body.link == "") res.send({
+        "status": "Ссылка не может быть пустой"
+    });
+
+    let short_link = make_short_link(5)
+    let date_now = new Date;
+
+    let data = db.create_obj("links",{
+        "link": req.body.link,
+        "key": short_link,
+        "time": date_now.toString().split('GMT')[0],
+        "transitions": 0
+    });
+
+    res.send({
+        "status": "ok",
+        "shortlink": short_link
+    });
+})
+
+
+app.listen(port, () => {
+    console.log('Server started...');
+});
---
+++ public/js/index.js
@@ -0,0 +1,99 @@
+let protocol = "http://";
+let protocol_red = "https://";
+
+async function check() {
+    if (window.location.search.slice(1).split(',')[0].split('=')[0] == '') {
+        create_form()
+    } else {
+        let response = await fetch('/check', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                link: window.location.search.slice(1).split(',')[0].split('=')[0]
+            })
+        })
+
+        let data = await response.json();
+
+        if (data.data == null) {
+            document.querySelector("#message").innerHTML = "Такой ссылки нет, или она удаленна :("
+            document.querySelector("#to_home").innerHTML = "Создать ссылку"
+        } else {
+            if (data.data[0]['link'].indexOf('http') == 0) {
+                location.replace(data.data[0]['link'])
+            } else {
+                location.replace(protocol_red+data.data[0]['link'])
+            }
+        }
+    }
+}
+
+
+function create_form() {
+    let home_content = document.querySelector('#home_content')
+
+    let form = document.createElement('form');
+    form.className = `form`;
+
+    let link_input = document.createElement('input');
+    link_input.className = `inp`;
+    link_input.id = `link`;
+    link_input.placeholder = `Ссылка`;
+
+
+    let btn = document.createElement('button');
+    btn.className = `btn`;
+    btn.id = `btn_create_link`;
+    btn.innerHTML = `Создать ссылку`;
+    btn.onclick = create_link
+
+    form.appendChild(link_input);
+    form.appendChild(btn);
+    home_content.appendChild(form);
+
+
+    home_content.addEventListener("submit", create_link, true);
+}
+
+check()
+
+
+async function create_link(event) {
+    event.preventDefault();
+
+    function isValidUrl(url) {
+        var objRE = /(^https?:\/\/)?[a-z0-9~_\-\.]+\.[a-z]{2,9}(\/|:|\?[!-~]*)?$/i;
+        return objRE.test(url);
+    }
+
+    if (!isValidUrl(document.querySelector('#link').value)) {
+        document.querySelector('#alert_message').innerHTML = "Некорректная ссылка"
+        document.querySelector('#new_link').innerHTML = "";
+    } else {
+        let response = await fetch('/new', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                link: document.querySelector('#link').value
+            })
+        })
+
+        let data = await response.json();
+
+        if (data.status == null) {
+            alert('Error')
+        } else if (data.status == "Ссылка не может быть пустой") {
+            document.querySelector('#alert_message').innerHTML = "Ссылка не может быть пустой"
+            document.querySelector('#new_link').innerHTML = "";
+        } else if (data.status == 'ok') {
+            document.querySelector('#alert_message').innerHTML = ""
+            document.querySelector('#new_link').innerHTML = window.location.host + '/?' + data.shortlink;
+            document.querySelector('#new_link').href = protocol + window.location.host + '/?' + data.shortlink;
+        }
+
+    }
+}
